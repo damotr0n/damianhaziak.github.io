4eba1aaa4e974bc6b12338ddafe0848ffaae1793
Damian Haziak <dmnhaziak3@gmail.com>
2021-02-15 21:33:33 +0000

    extracted data from content pages, left one for template creation

--- src/components/ProjectsContent.tsx
+++ src/components/ProjectsContent.tsx
@@ -16,63 +16,6 @@ function ProjectsContent() {
                     </div>
                 </a>
 
-                <a href="https://github.com/DamianHaziak/ray-tracing" target="_blank">
-                    <div className="card">
-                        <img className="card-img"  src="images/IMGPM.png" alt="Ray Tracing Example" />
-                        <h3 className="card-title"> Ray Tracing </h3>
-                        <p className="card-description">Project where I got to implement a ray tracing based renderer including: photon mapping, reflection, refraction, and diffusion. The image above is an example of a rendered image.</p>
-                    </div>
-                </a>
-
-                <a href="https://github.com/DamianHaziak/damianhaziak.github.io" target="_blank">
-                    <div className="card">
-                        <img className="card-img" src="images/personal-portfolio.png" alt="personal portfolio" />
-                        <h3 className="card-title">Personal Portfolio webpage</h3>
-                        <p className="card-description">
-                            This webpage, created in my spare time.
-                        </p>
-                    </div>
-                </a>
-
-                <a href="https://github.com/DamianHaziak/WebGLObjectSandbox" target="_blank">
-                    <div className="card">
-                        <img className="card-img" src="images/WebGL.png" alt="WebGL render of a Rubik's cube" />
-                        <h3 className="card-title">WebGL Object Sandbox</h3>
-                        <p className="card-description">
-                            A university coursework project that uses <code>three.js</code> to render and view some objects.
-                        </p>
-                    </div>
-                </a>
-
-                <a href="https://codepen.io/collection/AyGOzb" target="_blank">
-                    <div className="card">
-                        <img className="card-img" src="images/fcc-reswebdes.png" alt="responsive web design project collection" />
-                        <h3 className="card-title">Responsive Web Design Certification</h3>
-                        <p className="card-description">
-                            A series of small projects created to obtain the Responsive Web Design Certification from FreeCodeCamp.
-                        </p>
-                    </div>
-                </a>
-
-                <a href="https://codepen.io/collection/Dommge" target="_blank">
-                    <div className="card">
-                        <img className="card-img" src="images/fcc-frontenddev.png" alt="WebGL render of a Rubik's cube" />
-                        <h3 className="card-title">Front End Libraries Certification</h3>
-                        <p className="card-description">
-                            A series of small projects, created using React, to obtain the Front End Development Certification from FreeCodeCamp.
-                        </p>
-                    </div>
-                </a>
-
-                <a href="https://github.com/DamianHaziak/Covid19-UK-Dashboard" target="_blank">
-                    <div className="card">
-                        <img className="card-img" src="images/UKCOV19.png" alt="WebGL render of a Rubik's cube" />
-                        <h3 className="card-title">UK COVID-19 current cases as percentage of cases</h3>
-                        <p className="card-description">
-                            Small curiosity project to visualize the percentage of COVID-19 cases in the UK as a percentage of all the tests done.
-                        </p>
-                    </div>
-                </a>
             </div>
         </>
     );
