357e5a6f61b4cf9bdce4d53313dbc55722ef466e
Damian Haziak <dmnhaziak3@gmail.com>
2021-02-18 19:18:21 +0000

    set up contact card tsx

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,18 @@
+
+function ContactCard(props: {
+    href: string,
+    svg_path: string,
+    view_box: string,
+    card_title: string
+}){
+    return (
+        <a href={props.href} target="_blank">
+            <div className="card-contact">
+                <svg enableBackground={"new " + props.view_box} height="512" viewBox={props.view_box} width="512" xmlns="http://www.w3.org/2000/svg"><path d={props.svg_path}/></svg>
+                <h3 className="card-title">{props.card_title}</h3>
+            </div>
+        </a>
+    )
+}
+
+export default ContactCard
--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import CardWrapper from './CardWrapper'
+import ContactCard from './ContactCard'
 
-// TODO: convert to components
+interface Icontact {
+    href: string;
+    svg_path: string;
+    view_box: string;
+    card_title: string;
+}
+
 function ContactContent() {
+
+    const [contacts, setContacts] = useState<JSX.Element[]>()
+
+    useEffect(() => {
+        
+        fetch('data/contact.json',
+        {
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then(myJson => {
+            console.log(myJson)
+            const projectsList = myJson.map((contact: Icontact) => 
+                <ContactCard 
+                    href={contact.href}
+                    svg_path={contact.svg_path}
+                    view_box={contact.view_box}
+                    card_title={contact.card_title}
+                />
+            );
+            setContacts(projectsList)
+        });
+
+    }, [])
+
     return (
-        <>
-            <h2 className="main-title">Contact</h2>
-            <p className="subtitle">Click on each icon to be sent to that webpage.</p>
-            <div className="card-container contact-card-container">
-
-                <a href="mailto:dmnhaziak3@gmail.com" target="_blank">
-                    <div className="card-contact">
-                        <svg enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m467 61h-422c-24.813 0-45 20.187-45 45v300c0 24.813 20.187 45 45 45h422c24.813 0 45-20.187 45-45v-300c0-24.813-20.187-45-45-45zm-65.19 30-145.81 108.785-145.81-108.785zm-356.81 0h15.021l187.009 139.523c2.661 1.985 5.815 2.977 8.97 2.977s6.309-.992 8.97-2.977l187.009-139.523h15.021c8.265 0 14.989 6.719 14.999 14.982l-225.999 168.796-225.999-168.796c.01-8.263 6.734-14.982 14.999-14.982zm202.024 214.518c2.662 1.988 5.819 2.982 8.976 2.982s6.314-.994 8.976-2.982l157.024-117.28v232.762h-332v-232.762zm-217.024 100.482v-262.575l30 22.407v255.168h-15c-8.271 0-15-6.729-15-15zm437 15h-15v-255.168l30-22.407v262.575c0 8.271-6.729 15-15 15z"/></svg>
-                        <h3 className="card-title">Email Me!</h3>
-                    </div>
-                </a>
-
-            </div>
-        </>
+        <CardWrapper 
+            mainTitle={"Contact"}
+            subtitle={"Click on each icon to be sent to that webpage."}
+        >
+            {contacts}
+        </CardWrapper>
     );
 }
 
