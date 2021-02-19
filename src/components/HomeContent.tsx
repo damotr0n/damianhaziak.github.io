ad211014921a68f93e2dd8663fd697889b15c2a1
Damian Haziak <dmnhaziak3@gmail.com>
2021-02-19 18:27:10 +0000

    removed hardcoded animation

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 function HomeContent(){
     return(
         <>
-            <h1 className="fall-down-animation">Damian Haziak</h1>
-            <p className="fall-down-animation">Graduate Developer and Consultant looking for jobs in web development.</p>
-            <div id="home-image" className="fall-down-animation">
+            <h1>Damian Haziak</h1>
+            <p>Graduate Developer and Consultant looking for jobs in web development.</p>
+            <div id="home-image">
                 <span className="material-icons front-page-icon" id="desktop-windows">desktop_windows</span>
                 <span className="material-icons front-page-icon" id="keyboard">keyboard</span>
                 <span className="material-icons front-page-icon" id="design-services">design_services</span>
