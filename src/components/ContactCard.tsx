346e51a0b868f4ff1b55b8ee6885362dba02dc5c
Damian Haziak <dmnhaziak3@gmail.com>
2021-02-20 17:11:36 +0000

    fixed noreferrer issue

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -6,7 +6,7 @@ function ContactCard(props: {
     card_title: string
 }){
     return (
-        <a className="contact-card-container-anchor" href={props.href} target="_blank">
+        <a className="contact-card-container-anchor" href={props.href} target="_blank" rel="noreferrer">
             <div className="card-contact">
                 <svg enableBackground={"new " + props.view_box} height="512" viewBox={props.view_box} width="512" xmlns="http://www.w3.org/2000/svg"><path d={props.svg_path}/></svg>
                 <h3 className="card-title">{props.card_title}</h3>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,7 +7,7 @@ function ProjectCard(props: {
     desc: string
 }){
     return(
-        <a href={props.href} target="_blank">
+        <a href={props.href} target="_blank" rel="noreferrer">
             <div className="card">
                 <img className="card-img" src={props.imgSrc} alt={props.imgAlt} />
                 <h3 className="card-title">{props.title}</h3>
