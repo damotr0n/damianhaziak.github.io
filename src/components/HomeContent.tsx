04ec09fd3e287b3665dc76c71c579eb6cead9848
Damian Haziak <dmnhaziak3@gmail.com>
2021-02-22 19:22:06 +0000

    added big head, adjusted css

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BigHead} from '@bigheads/core';
 
 function HomeContent(){
     return(
@@ -6,9 +7,28 @@ function HomeContent(){
             <h1>Damian Haziak</h1>
             <p>Graduate Developer and Consultant looking for jobs in web development.</p>
             <div id="home-image">
-                <span className="material-icons front-page-icon" id="desktop-windows">desktop_windows</span>
-                <span className="material-icons front-page-icon" id="keyboard">keyboard</span>
-                <span className="material-icons front-page-icon" id="design-services">design_services</span>
+                <BigHead
+                    accessory="shades"
+                    body="chest"
+                    circleColor="blue"
+                    clothing="shirt"
+                    clothingColor="white"
+                    eyebrows="raised"
+                    eyes="happy"
+                    faceMask={false}
+                    faceMaskColor="red"
+                    facialHair="none3"
+                    graphic="react"
+                    hair="short"
+                    hairColor="black"
+                    hat="none"
+                    hatColor="red"
+                    lashes
+                    lipColor="pink"
+                    mask
+                    mouth="openSmile"
+                    skinTone="light"
+                />
             </div>
         </>
     );
