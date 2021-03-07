ee81662b14f1ddf6d67f05b7c53d6bfa95398265
Damian Haziak <dmnhaziak3@gmail.com>
2021-03-07 13:07:00 +0000

    added hamburger react

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavbarListItem from './NavbarListItem';
+import { Squash as Hamburger } from 'hamburger-react'
 
 function Navbar(props: {menuItems: string[], callback: any}){
 
     var menu = props.menuItems.map(name => <NavbarListItem title={name} callback={props.callback}/> );
+    const [isOpen, setOpen] = useState(false);
 
     return(
         <div id="navbar">
@@ -11,9 +13,11 @@ function Navbar(props: {menuItems: string[], callback: any}){
                 <ul id="navbar-item-wrapper" className="topnav">
                     {menu}
                 </ul>
-                <a>
-                    <span className="material-icons" id="navbar-menu">menu</span>
-                </a>
+                <Hamburger 
+                    toggled={isOpen} 
+                    toggle={setOpen} 
+                    color={'#fff'}
+                />
             </div>  
         </div>
     );
