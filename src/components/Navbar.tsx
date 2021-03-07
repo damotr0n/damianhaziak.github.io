0632d404226e78684a9b24ecc71a49fef204600f
Damian Haziak <dmnhaziak3@gmail.com>
2021-03-07 14:18:30 +0000

    fix style hover issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,17 +5,29 @@ import { Squash as Hamburger } from 'hamburger-react'
 function Navbar(props: {menuItems: string[], callback: any}){
 
     var menu = props.menuItems.map(name => <NavbarListItem title={name} callback={props.callback}/> );
+    
     const [isOpen, setOpen] = useState(false);
+    const [navbarHeight, setNavbarHeight] = useState({height: "6vh"});
+
+    const toggleNavbar = () => {
+        if(isOpen){
+            setOpen(false)
+            setNavbarHeight({ height: "6vh" })
+        } else {
+            setOpen(true)
+            setNavbarHeight({ height: "100vh"})
+        }
+    }
 
     return(
-        <div id="navbar">
+        <div id="navbar" style={navbarHeight}>
             <div id="navbar-ul-wrapper">
                 <ul id="navbar-item-wrapper" className="topnav">
                     {menu}
                 </ul>
                 <Hamburger 
                     toggled={isOpen} 
-                    toggle={setOpen} 
+                    toggle={toggleNavbar}
                     color={'#fff'}
                 />
             </div>  
